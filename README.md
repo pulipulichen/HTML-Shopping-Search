a13bb17f7691e3792411e8ddb5c28fbe82fada39
Pulipuli Chen <blog@pulipuli.info>
2021-02-20 02:07:57 +0800

    0220 第一版搜尋功能完成

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # HTML-Shopping-Search
 Searching several shopping websites from a input form.
+
+https://pulipulichen.github.io/HTML-Shopping-Search/
+
+https://www.flaticon.com/free-icon/buy_825575
---
+++ components/app-main/app-main.js
@@ -0,0 +1,131 @@
+/* global ClipboardUtils */
+
+let appMain = {
+  data () {
+    let websitesList = [
+      'Shopee',
+      'Ruten',
+      'Yahoo Bid',
+      'Yahoo Buy',
+      'Yahoo Mall',
+      'Momo',
+      'PChome Shopping'
+    ]
+    
+    return {
+      cacheKey: 'HTML-Shopping-Search',
+      cacheAttrs: ['keyword', 'websites'],
+      init: false,
+      
+      websitesList,
+      tableContent: `網址	品項	價位(含運費)	備註`,
+      
+      keyword: 'Android 電子紙',
+      websites: websitesList
+    }
+  },
+  mounted () {
+    this.dataLoad()
+    
+    this.inited = true
+  },
+  watch: {
+    keyword () {
+      this.dataSave()
+    },
+    websites () {
+      this.dataSave()
+    },
+  },
+  computed: {
+    computedFilename () {
+      return (new Date()).yyyymmdd() + ' ' + this.keyword
+    },
+    encodeKeyword () {
+      return encodeURIComponent(this.keyword)
+    },
+    computedShopeeURL () {
+      return `https://shopee.tw/search/?keyword=${this.encodeKeyword}&order=asc&page=0&sortBy=price`
+    },
+    computedRutenURL () {
+      return `https://find.ruten.com.tw/s/?q=${this.encodeKeyword}&sort=prc%2Fac`
+    },
+    computedYahooBidURL () {
+      return `https://tw.bid.yahoo.com/search/auction/product?p=${this.encodeKeyword}&sort=curp`
+    },
+    computedYahooBuyURL () {
+      return `https://tw.buy.yahoo.com/search/product?p=${this.encodeKeyword}&sort=price`
+    },
+    computedYahooMallURL () {
+      return `https://tw.mall.yahoo.com/search/product?p=${this.encodeKeyword}&sort=p`
+    },
+    computedMomoURL () {
+      return `https://www.momoshop.com.tw/search/searchShop.jsp?keyword=${this.encodeKeyword}&searchType=2&curPage=1&_isFuzzy=0&showType=chessboardType`
+    },
+    computedPChomeShoppingURL () {
+      return `https://ecshweb.pchome.com.tw/search/v3.3/?q=${this.encodeKeyword}&scope=all&sortParm=prc&sortOrder=ac`
+    },
+    websiteURLMapping () {
+      return {
+        'Shopee': this.computedShopeeURL,
+        'Ruten': this.computedRutenURL,
+        'Yahoo Bid': this.computedYahooBidURL,
+        'Yahoo Buy': this.computedYahooBuyURL,
+        'Yahoo Mall': this.computedYahooMallURL,
+        'Momo': this.computedMomoURL,
+        'PChome Shopping': this.computedPChomeShoppingURL
+      }
+    },
+    urlList () {
+      let list = []
+      
+      this.websites.forEach(website => {
+        list.push(this.websiteURLMapping[website])
+      })
+      
+      return list
+    }
+  },
+  methods: {
+    dataLoad () {
+      let projectFileListData = localStorage.getItem(this.cacheKey)
+      if (!projectFileListData) {
+        return false
+      }
+      
+      projectFileListData = JSON.parse(projectFileListData)
+      for (let key in projectFileListData) {
+        this[key] = projectFileListData[key]
+      }
+    },
+    dataSave () {
+      if (this.inited === false) {
+        return false
+      }
+      
+      let keys = this.cacheAttrs
+      
+      let data = {}
+      keys.forEach(key => {
+        data[key] = this[key]
+      })
+      
+      data = JSON.stringify(data)
+      localStorage.setItem(this.cacheKey, data)
+    },
+    searchShoppingWebsite () {
+      
+      this.urlList.forEach(url => {
+        window.open(url, (new URL(url)).host)
+      })
+    },
+    copyFilename () {
+      ClipboardUtils.copyPlainString(this.computedFilename)
+    },
+    copyTable () {
+      ClipboardUtils.copyPlainString(this.tableContent)
+    }
+  }
+}
+
+module.exports = appMain
